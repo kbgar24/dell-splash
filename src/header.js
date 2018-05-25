ad7e3f5d17fd1668ea90443c0859e4ce2e237e83
Kendrick Gardner <kbgar24@gmail.com>
2018-05-25 00:19:50 -0500

    Style logo header

--- src/header.js
+++ src/header.js
@@ -1,22 +1,30 @@
 import React from 'react';
 
-export default class Header extends Component {
-  
-  render(){
-    <div>
-      <div className='finLogo'>FinExpert</div>
+const Header = () => (
+  <div>
+    <div className='fin-logo'>[FinExpert]</div>
+    <div className='icon1'>
+      <img src='/assets/img/location-icon.png'/>
       <div>
-        <span>267 Park Avenue</span>
-        <span>New York, NY 90210</span>
-      </div>
-      <div>
-        <span>(123) 45678910</span>
-        <span>info@demolink.com</span>
-      </div>
-      <div>
-        <span>Mon - Sat 0900 - 1800</span>
-        <span>Sunday CLOSED</span>
+        <div>267 Park Avenue</div>
+        <div>New York, NY 90210</div>
       </div>
     </div>
-  }
-}
+    <div className='icon2'>
+      <img src='/assets/img/phone-icon.png' />
+      <div>
+        <div>(123) 45678910</div>
+        <div>info@demolink.com</div>
+      </div>
+    </div>
+    <div className='icon3'>
+      <img src='/assets/img/clock-icon.png' />
+      <div>
+        <div>Sunday CLOSED</div>
+        <div>Mon - Sat 0900 - 1800</div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Header;
